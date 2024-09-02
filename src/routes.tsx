98b6cb564d033211ae33d621019acbaba9e9bd53
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2024-09-01 21:37:22 -0300

    feat: add not found route

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
-import { AppLayout } from "./pages/_layout/app";
+import { NotFound } from "./pages/404";
+import { AppLayout } from "./pages/_layouts/app";
+import { AuthLayout } from "./pages/_layouts/auth";
 import { Home } from "./pages/app/home";
+import { AuthPage } from "./pages/auth/auth-page";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [{ path: "/", element: <Home /> }],
+  },
+  {
+    path: "/",
+    element: <AuthLayout />,
+    children: [{ path: "/auth/sign-in", element: <AuthPage /> }],
   },
 ]);
