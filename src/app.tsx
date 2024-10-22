9bda7238f9d43e8165e0e7c01c0c02eadbef3d37
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2024-10-22 14:48:01 -0300

    feat: add toast in app

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,19 @@
-import { Helmet, HelmetProvider } from "react-helmet-async";
-import { RouterProvider } from "react-router-dom";
-import { ThemeProvider } from "./components/theme/theme-provider";
-import { router } from "./routes";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "./lib/query";
+import { QueryClientProvider } from '@tanstack/react-query';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { RouterProvider } from 'react-router-dom';
+import { ThemeProvider } from './components/theme/theme-provider';
+import { Toaster } from './components/ui/toaster';
+import { queryClient } from './lib/query';
+import { router } from './routes';
 
 export function App() {
   return (
     <HelmetProvider>
       <QueryClientProvider client={queryClient}>
-        <ThemeProvider storageKey="rabbitlink-theme" defaultTheme="light">
-          <Helmet titleTemplate="%s | rabbit.link" />
+        <ThemeProvider storageKey='rabbitlink-theme' defaultTheme='light'>
+          <Helmet titleTemplate='%s | rabbit.link' />
           <RouterProvider router={router} />
+          <Toaster />
         </ThemeProvider>
       </QueryClientProvider>
     </HelmetProvider>
